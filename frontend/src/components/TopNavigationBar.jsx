e7ce26f16aedf50f4c28ac2141668ef84ec381ff
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-06-15 17:33:05 -0400

    add favbadge to topnavbar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TopicList from './TopicList';
 import '../styles/TopNavigationBar.scss';
+import FavBadge from './FavBadge';
 
 const TopNavigation = (props) => {
   const { topics } = props;
@@ -8,6 +9,7 @@ const TopNavigation = (props) => {
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics= {topics} />
+      <FavBadge />
     </div>
   )
 }
